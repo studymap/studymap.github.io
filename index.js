484645f331c0a3c9a2344c8c33920490af077f3d
studymap <66041453+studymap@users.noreply.github.com>
2020-05-31 14:58:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
     <script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
     <script type='text/javascript'>    
-      var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTZSYtvEbKtx8ktFapoewsBmyZPdOIoSC6OOZUDmr38p-h81IvyfOMbHMP7R9Cxc-3HZwVGR-ASIwj4/pubhtml';
+      var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRURHOfPPygsi3KTkLl1hGvap5dtsgZrACkkPrLMJveEKHLp7b6wQOxy8BYwskOhCMkeNRZQdniU1LL/pubhtml';
 
       function init() {
         Tabletop.init( { key: publicSpreadsheetUrl,
