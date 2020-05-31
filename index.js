30ecd138e7c3417833f650655976ef29974e7b78
studymap <66041453+studymap@users.noreply.github.com>
2020-05-31 15:03:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
     <script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
     <script type='text/javascript'>    
-      var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRURHOfPPygsi3KTkLl1hGvap5dtsgZrACkkPrLMJveEKHLp7b6wQOxy8BYwskOhCMkeNRZQdniU1LL/pubhtml';
+      var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1DtWo_Yi53PQRfQk3pyIpK0_NrKkYK54c09oHs2a4VZo/edit?usp=sharing';
 
       function init() {
         Tabletop.init( { key: publicSpreadsheetUrl,
