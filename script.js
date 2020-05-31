3f8e272e760e4fbc52161becc8112a3179b5f388
studymap <66041453+studymap@users.noreply.github.com>
2020-05-31 15:32:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,3 @@ console.log(data)
 simpleSheet: true } )
 }
 window.addEventListener(‘DOMContentLoaded’, init)
-
-for (var i = 0; i < array.length; i++) {
-array[i]
-}
-
-for (var i = 0; i < data.length; i++) {
-$(‘.post’).append(
-‘<div class=”article” style=”background-image: url( ‘ +
-data[i].img + ‘.jpg)”>’ +
-‘<div class=”text”>’ +
-‘<h1>’ + data[i].title + ‘</h1>’+
-data[i].body +
-‘</div> ‘ + ‘<div class=”meta-info”>’ + ‘<img src =”’ + data[i].img + ‘.jpg”>’+
-‘<div class=”date”>’ + data[i].date + ‘</div>’ +
-‘</div>’ +
-‘</div>’);
-}
