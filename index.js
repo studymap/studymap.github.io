58d9cdd70cfe3d72547f2303bdf82fd8e7498951
studymap <66041453+studymap@users.noreply.github.com>
2020-05-31 14:22:23 +0900

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+$ git clone https://github.com/jsoma/tabletop.git
+function init() {
+Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1DtWo_Yi53PQRfQk3pyIpK0_NrKkYK54c09oHs2a4VZo/pubhtml',
+callback: function(data, tabletop) {
+console.log(data)
+},
+simpleSheet: true } )
+}
+window.addEventListener(‘DOMContentLoaded’, init)
