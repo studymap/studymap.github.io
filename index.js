2c3324969aaacc1038d71c5fc6b9f1cd9545df10
studymap <66041453+studymap@users.noreply.github.com>
2020-05-31 14:34:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 $ git clone https://github.com/jsoma/tabletop.git
 function init() {
-Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1DtWo_Yi53PQRfQk3pyIpK0_NrKkYK54c09oHs2a4VZo/pubhtml',
-callback: function(data, tabletop) {
-console.log(data)
-},
-simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
+          Papa.parse('https://docs.google.com/spreadsheets/d/1DtWo_Yi53PQRfQk3pyIpK0_NrKkYK54c09oHs2a4VZo/pub?output=csv', {
+          download: true,
+          header: true,
+          complete: function(results) {
+            var data = results.data
+            console.log(data)
+          }
+        })
