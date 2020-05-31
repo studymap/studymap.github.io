03c191881f142297de8176797d9ceb650c1b0f71
studymap <66041453+studymap@users.noreply.github.com>
2020-05-31 14:39:41 +0900

    Update and rename index.js to index.md

--- index.js
+++
@@ -1,9 +0,0 @@
-function init() {
-  Tabletop.init( {
-    key: 'https://docs.google.com/spreadsheets/d/1DtWo_Yi53PQRfQk3pyIpK0_NrKkYK54c09oHs2a4VZo/pubhtml',
-    simpleSheet: true }
-  ).then(function(data, tabletop) { 
-    console.log(data)
-  })
-}
-window.addEventListener('DOMContentLoaded', init)
