3d22143360519c44ffc39b2edefe0b41b9b5aba7
studymap <66041453+studymap@users.noreply.github.com>
2020-05-31 15:06:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
     <script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
     <script type='text/javascript'>    
       var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1sbyMINQHPsJctjAtMW0lCfLrcpMqoGMOJj6AN-sNQrc/pubhtml';
